089228d22dc0527d187e57f6bccbc75b68eb5b3c
vitertik <cF!%eq34>
2024-09-19 23:58:04 -0400

    Add favicon, return content from backend

--- main.go
+++ main.go
@@ -7,32 +7,35 @@ import (
 	"text/template"
 )
 
-type Film struct {
-	Title    string
-	Director string
+type PageContent struct {
+	Header           string
+	Tel              string
+	H1               string
+	Paragraph        string
+	ProjectsTitle    string
+	ProjectsSubTitle string
 }
 
 func main() {
 	fmt.Println("Привіт мій друже!")
 
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-
-		tmpl := template.Must(template.ParseFiles("index.html"))
-
-		films :=map[string][]Film{
-			"Films": {
-				{Title: "First title", Director: "1st Director"},
-				{Title: "Second title", Director: "2nd Director"},
-				{Title: "Third title", Director: "3rd Director"},
-			},
+		tmpl, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			log.Fatal("Error parsing template: ", err)
 		}
-		tmpl.Execute(w, films)
 
-		// for learning purpose here
-		// tmpl := template.Must(template.ParseFiles("index.html"))
-		// tmpl.Execute(w, nil)
-
-		// io.WriteString(w, "Hello YO")
+		content := PageContent{
+			Header:           "For a Demo, Simply text « DEMO » at",
+			Tel:              "1 (855) 950-4006",
+			H1:               "Convert your missed calls into business opportunities",
+			Paragraph:        "Coming soon to a phone system near you!",
+			ProjectsTitle:    "Every. Call. Matters.",
+			ProjectsSubTitle: "Create seamless engagement with your callers at all times. Whether you are open or closed, free to pick up the phone or busy, make every call count.",
+		}
+		tmpl.Execute(w, content)
 	}
 	http.HandleFunc("/", h1)
 
